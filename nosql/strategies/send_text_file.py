d1b0f1f0bf2163dc037daacc11e80d1bbe819db4
smallpoxlviv <walterwhite.99.100@gmail.com>
2022-06-21 20:48:19 +0300

    finish maybe

--- nosql/strategies/send_text_file.py
+++ nosql/strategies/send_text_file.py
@@ -6,6 +6,6 @@ from .send_text import SendText
 class SendTextFile(SendText):
 
     def send_text(self, file_name: str, text_list: list):
-        with open(f'../resources/', 'w+') as f:
+        with open(f'../resources/{file_name}', 'w+') as f:
             for text in text_list:
                 f.write(f'{json.dumps(text)}\n')
